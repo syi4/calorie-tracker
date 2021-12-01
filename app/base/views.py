d91d9df2fd0c29fb00d6ea5cb18b0073db07426a
yisy87 <72623292+yisy87@users.noreply.github.com>
2021-12-01 05:02:28 -0500

    added delete button

--- app/base/views.py
+++ app/base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import *
 
 def index(request):
@@ -15,3 +15,10 @@ def index(request):
         foods = Food.objects.all()
     consumed_food = Consume.objects.filter(user=request.user)
     return render(request, 'myapp/index.html', {'foods':foods, 'consumed_food':consumed_food})
+
+def delete_consume(request, id):
+    consumed_food = Consume.objects.get(id=id)
+    if request.method == 'POST':
+        consumed_food.delete()
+        return redirect('/')
+    return render(request, 'myapp/delete.html')
