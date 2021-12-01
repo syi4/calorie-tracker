7ce0b9a22865de93485ffa49a7ac926cf8a6e432
yisy87 <72623292+yisy87@users.noreply.github.com>
2021-12-01 04:03:18 -0500

    first commit

---
+++ app/base/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import *
+
+def index(request):
+
+    if request.method == 'POST':
+        food_consumed = request.POST['food_consumed']
+        consume = Food.objects.get(name=food_consumed)
+        user = request.user
+        consume = Consume(user=user, food_consumed=consume)
+        consume.save()
+        foods = Food.objects.all()
+
+    else:
+        foods = Food.objects.all()
+    consumed_food = Consume.objects.filter(user=request.user)
+    return render(request, 'myapp/index.html', {'foods':foods, 'consumed_food':consumed_food})
